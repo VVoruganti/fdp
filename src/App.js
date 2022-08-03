3ad9681faefeddb492c22ee0630e12bd6d8aa3b6
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2022-08-02 22:43:27 -0700

    Basic UI with buttons and text

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import { MantineProvider, Text, Title, Button, Group, Container } from '@mantine/core';
+
 export function App() {
-    return <h1>Hello world!</h1>;
+    return (
+        <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+        >
+            <div className="app" style={{ width: 350, paddingBottom: 25 }}>
+                <Container>
+                    <Title align="center">Fair Data Society</Title>
+                    <Text align="center">
+                        Use this site to save websites
+                    </Text>
+                    <Group position="center">
+                        <Button>Save</Button>
+                        <Button>View</Button>
+                    </Group>
+                </Container>
+            </div>
+        </MantineProvider>
+    );
 }
